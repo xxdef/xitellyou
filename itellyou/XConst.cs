26f6ef54248f5e2e6b197cb326f798895cba6ed7
liaowuping <348984539@qq.com>
2017-07-19 19:35:26 +0800

    init

---
+++ itellyou/XConst.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itellyou
+{
+	public static class XConst
+	{
+		public static readonly string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36";
+
+		/// <summary>
+		/// 操作系统
+		/// </summary>
+		public static readonly string OS = "7ab5f0cb-7607-4bbe-9e88-50716dc43de6";
+    }
+}
